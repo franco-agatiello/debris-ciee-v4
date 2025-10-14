ba084fd2512fdae60a99d2b3993bdfe271bc3534
Franco Rocco Agatiello <121842945+franco-agatiello@users.noreply.github.com>
2025-10-14 00:15:49 -0300

    Delete suscripcion directory

--- suscripcion/backend/db.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./suscriptores.db');
-
-// Ejecutar el esquema al iniciar
-const fs = require('fs');
-const schema = fs.readFileSync(__dirname + '/schema.sql', 'utf8');
-db.exec(schema);
-
-module.exports = db;
